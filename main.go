254f602a70e76585d473d8d68569e58066c98481
Shihua Cheng <shihuac2@illinois.edu>
2023-09-30 01:02:12 -0500

    refactoring

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ func main() {
 		http.ServeFile(w, r, "html/index.html")
 	})
 
-	// Serve the images from the "images" folder
+	// Serve files
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
 
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
